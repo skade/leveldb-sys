262cf9eeb5b40edcb8297cd1635b7c45a897e8a5
Alexander Rodin <rodin.alexander@gmail.com>
2019-07-27 09:19:56 +0000

    Link to libstdc++ only on GNU hosts

--- src/build.rs
+++ src/build.rs
@@ -189,7 +189,7 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("apple") {
         println!("cargo:rustc-link-lib=c++");
-    } else {
+    } else if target.contains("gnu") {
         println!("cargo:rustc-link-lib=stdc++");
     }
 
